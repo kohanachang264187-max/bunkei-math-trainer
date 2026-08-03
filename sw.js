eba7f6816d7a360e8f0a55e7240e5912e3d3970a
kohanachang264187-max <kohanachang264187@gmail.com>
2026-08-03 21:09:34 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bunkei-math-v04";
+const CACHE = "bunkei-math-v05";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 self.addEventListener("install", event => {
   self.skipWaiting();
